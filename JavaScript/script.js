e71d18f5674da682bd850a14825a88e949a31d6a
Sakib Hasan <sakib100.sa@gmail.com>
2021-11-07 13:21:28 +0600

    finally done my first project with JavaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -11,62 +11,138 @@ let minusBtnCase = document.getElementsByClassName("fa-minus")[1];
 let quantityCase = parseFloat(document.getElementsByClassName("quantity")[1].value);
 let caseAmount = parseFloat(document.getElementsByTagName("span")[1].innerText);
 
+// amount variable
+let subTotal = parseFloat(document.getElementById("subtotal").innerText);
+let total = parseFloat(document.getElementById("total").innerText);
+let tax = parseFloat(document.getElementById("tax").innerText);
+
 // phone quantity plus
-plusBtnMobile.addEventListener("click", function () {
+plusBtnMobile.addEventListener("click", function phoneQuantityPlus() {
 
     quantityMobile = quantityMobile + 1;
 
-    document.getElementsByClassName("quantity")[0].value = parseFloat(quantityMobile)
+    document.getElementsByClassName("quantity")[0].value = quantityMobile
     let totalMobileAmount = mobileAmount * quantityMobile;
-    document.getElementsByTagName("span")[0].innerText = totalMobileAmount;
-    return totalMobileAmount
+    document.getElementsByTagName("span")[0].innerText = parseFloat(totalMobileAmount);
+    subTotal = subTotal + 1219;
+    return document.getElementById("subtotal").innerText = parseFloat(subTotal);
+
+
+
+})
+plusBtnMobile.addEventListener("click", function phoneQuantityPlus() {
+
+    tax = (subTotal * 15) / 100;
+    return document.getElementById("tax").innerText = parseFloat(tax)
 
 })
 
+plusBtnMobile.addEventListener("click", function phoneQuantityPlus() {
+
+    total = subTotal + tax;
+    return document.getElementById("total").innerText = parseFloat(total);
+
+})
+
+
+
+
 // phone case plus
-plusBtnCase.addEventListener("click", function () {
+plusBtnCase.addEventListener("click", function caseQuantityPlus() {
 
     quantityCase = quantityCase + 1;
 
     document.getElementsByClassName("quantity")[1].value = parseFloat(quantityCase)
     let totalCaseAmount = caseAmount * quantityCase;
     document.getElementsByTagName("span")[1].innerText = totalCaseAmount;
-    return totalCaseAmount
+
+    subTotal = subTotal + 59;
+    return document.getElementById("subtotal").innerText = parseFloat(subTotal);
+
+
+
+
+})
+
+plusBtnCase.addEventListener("click", function caseQuantityPlus() {
+
+    tax = (subTotal * 15) / 100;
+    return document.getElementById("tax").innerText = parseFloat(tax)
+
+
+
+
+})
+plusBtnCase.addEventListener("click", function caseQuantityPlus() {
+
+    total = subTotal + tax;
+    return document.getElementById("total").innerText = parseFloat(total);
+
+
+
 
 })
 
 // phone quantity minus
 
-minusBtnMobile.addEventListener("click", function () {
+minusBtnMobile.addEventListener("click", function phoneQuantityMinus() {
 
     quantityMobile = quantityMobile - 1;
 
-    document.getElementsByClassName("quantity")[0].value = parseFloat(quantityMobile)
+    document.getElementsByClassName("quantity")[0].value = parseFloat(quantityMobile);
     let totalMobileAmount = mobileAmount * quantityMobile;
     document.getElementsByTagName("span")[0].innerText = totalMobileAmount;
-    return totalMobileAmount
+    subTotal = subTotal - 1219;
+    return document.getElementById("subtotal").innerText = parseFloat(subTotal);
+
+
+})
+minusBtnMobile.addEventListener("click", function phoneQuantityMinus() {
+
+    tax = (subTotal * 15) / 100;
+    return document.getElementById("tax").innerText = parseFloat(tax);
+
+
+})
+
+minusBtnMobile.addEventListener("click", function phoneQuantityMinus() {
+
+    total = subTotal + tax;
+    return document.getElementById("total").innerText = parseFloat(total);
+
 
 })
 
 // case quantity minus 
-minusBtnCase.addEventListener("click", function () {
+minusBtnCase.addEventListener("click", function caseQuantityMinus() {
 
     quantityCase = quantityCase - 1;
 
     document.getElementsByClassName("quantity")[1].value = parseFloat(quantityCase);
     let totalCaseAmount = caseAmount * quantityCase;
     document.getElementsByTagName("span")[1].innerText = totalCaseAmount;
-    return totalCaseAmount
+    subTotal = subTotal - 59;
+    return document.getElementById("subtotal").innerText = parseFloat(subTotal);
+
+
+
+})
+minusBtnCase.addEventListener("click", function caseQuantityMinus() {
+
+    tax = (subTotal * 15) / 100;
+    return document.getElementById("tax").innerText = parseFloat(tax);
+
+
+
+
 
 })
 
+minusBtnCase.addEventListener("click", function caseQuantityMinus() {
 
+    total = subTotal + tax;
+    return document.getElementById("total").innerText = parseFloat(total);
 
+})
 
-
-let subTotal = parseFloat(document.getElementById("subtotal")[0, 1].innerText);
-
-subTotal = totalMobileAmount + totalCaseAmount;
-
-document.getElementById("subtotal")[0, 1].innerText = subTotal;
 
